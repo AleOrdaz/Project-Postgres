03cbb0c89259527f10684bd0f9f2b8b8532a5df6
Pilucas96 <diego_pilcar@hotmail.com>
2019-10-26 17:58:23 -0500

    USUARIOS
    FALTA EL TRIGER DE SUBTOTALES U ASIGNARLOS

--- Proyecto.sql
+++ Proyecto.sql
@@ -21,10 +21,6 @@ CREATE TABLE Almacen.Vendedor
 	CONSTRAINT PK_VENDEDOR PRIMARY KEY(IdVendedor)
 );
 
-
-INSERT INTO Almacen.Vendedor (Nombre,Domicilio,Email,Telefono,FechaNac) VALUES ('JUAN','DLJBKBKB','JH@BKHBKB','7866125678','05/10/1996')
-SELECT * FROM Almacen.Vendedor
-
 ALTER TABLE Almacen.Vendedor ADD CONSTRAINT UQ_EMAIL UNIQUE (Email);
 
 CREATE TABLE Transaccion.Cliente
@@ -37,10 +33,7 @@ CREATE TABLE Transaccion.Cliente
 	FechaNac DATE NOT NULL,
 	Edad INT,
 	CONSTRAINT PK_CLIENTE PRIMARY KEY(IdCliente)
-	
 );
-INSERT INTO Transaccion.Cliente (Nombre,Domicilio,Email,Telefono,FechaNac) VALUES ('DIEGO','DLJBKBKB','JH@BKHBKB','7866125678','05/10/1996')
-SELECT * FROM Transaccion.Cliente
 
 ALTER TABLE Transaccion.Cliente ADD CONSTRAINT UQ_EMAIL UNIQUE (Email);
 
@@ -52,8 +45,6 @@ CREATE TABLE Almacen.TipoProducto
 	CONSTRAINT PK_TIPOPRODUCTO PRIMARY KEY(IdTipoProducto)
 );
 
-INSERT INTO Almacen.TipoProducto(Nombre,Descripcion) VALUES ('PRUEBA','Alamo 22');
-SELECT * FROM Almacen.TipoProducto
 
 CREATE TABLE Almacen.Producto
 (
@@ -66,8 +57,6 @@ CREATE TABLE Almacen.Producto
 	CONSTRAINT FK_TIPOPRODUCTO FOREIGN KEY(IdTipoProducto) REFERENCES Almacen.TipoProducto(IdTipoProducto)
 );
 
-INSERT INTO Almacen.Producto (IdTipoProducto,Stock,Tamaño,Precio) VALUES (1,150,'Individual',500);
-SELECT * FROM Almacen.Producto;
 
 ALTER TABLE Almacen.Producto ADD CONSTRAINT CH_PRECIO CHECK (
 	Precio >= 100 AND Precio <= 500
@@ -91,9 +80,6 @@ CREATE TABLE Transaccion.Venta
 	CONSTRAINT FK_VENDEDOR FOREIGN KEY (IdVendedor) REFERENCES Almacen.Vendedor(IdVendedor) 
 );
 
-INSERT INTO Transaccion.Venta  (IdCliente,IdVendedor,Fecha) VALUES (1,1,'13/10/2019');
-SELECT * FROM Transaccion.Venta;
-
 CREATE TABLE Transaccion.DetalleVenta
 (
 	IdVenta BIGINT NOT NULL,
@@ -103,6 +89,7 @@ CREATE TABLE Transaccion.DetalleVenta
 	CONSTRAINT FK_VENTA2 FOREIGN KEY(IdVenta) REFERENCES Transaccion.Venta(IdVenta),
 	CONSTRAINT FK_PRODUCTO2 FOREIGN KEY(IdProducto) REFERENCES Almacen.Producto(IdProducto)
 );
+
 
 CREATE TABLE Almacen.Devolucion
  (
@@ -115,9 +102,6 @@ CREATE TABLE Almacen.Devolucion
 	CONSTRAINT FK_VENTA3 FOREIGN KEY(IdVenta) REFERENCES Transaccion.Venta(IdVenta)
  );
 
-INSERT INTO Almacen.Devolucion (IdVenta,Motivo,Fecha) VALUES (1,'PRUEBA','15/8/2019');
-SELECT * FROM Almacen.Devolucion;
-SELECT * FROM Almacen.Producto;
 
  CREATE TABLE Almacen.DetalleDevolucion
  (
@@ -128,13 +112,28 @@ SELECT * FROM Almacen.Producto;
 	CONSTRAINT FK_DEVOLUCCION FOREIGN KEY(IdDevolucion) REFERENCES Almacen.Devolucion(IdDevolucion),
 	CONSTRAINT FK_VENTA4 FOREIGN KEY(IdProducto) REFERENCES Almacen.Producto(IdProducto)
  );
- INSERT INTO Almacen.DetalleDevolucion (IdDevolucion,IdProducto,Cantidad) VALUES (1,1,150);
- SELECT * FROM Almacen.Producto;
- UPDATE Almacen.DetalleDevolucion SET cantidad = 200 where iddevolucion=1;
- DELETE FROM Almacen.DetalleDevolucion  WHERE iddevolucion=1 AND  idproducto=3;
- --triggers
 
+INSERT INTO Almacen.Vendedor (Nombre,Domicilio,Email,Telefono,FechaNac) VALUES ('Juanhgfds','DLJBKBKB','JH@jhgBOSI','7866125678','05/10/1994')
+INSERT INTO Transaccion.Cliente (Nombre,Domicilio,Email,Telefono,FechaNac) VALUES ('DIEGO','DLJBKBKB','JH@BKHBKB','7866125678','05/10/1996')
+INSERT INTO Almacen.TipoProducto(Nombre,Descripcion) VALUES ('PRUEBA','Alamo 22');
+INSERT INTO Transaccion.Venta  (IdCliente,IdVendedor,Fecha) VALUES (1,1,'13/10/2019')
+INSERT INTO Almacen.Devolucion (IdVenta,Motivo,Fecha) VALUES (1,'PRUEBA','15/8/2019')
+INSERT INTO Almacen.DetalleDevolucion (IdDevolucion,IdProducto,Cantidad) VALUES (1,3,150)
 
+SELECT * FROM Almacen.Vendedor
+SELECT * FROM Transaccion.Cliente
+SELECT * FROM Almacen.TipoProducto INSERT INTO Almacen.Producto (IdTipoProducto,Stock,Tamaño,Precio) VALUES (1,150,'Individual',500)
+SELECT * FROM Almacen.Producto
+SELECT * FROM Transaccion.Venta
+SELECT * FROM Almacen.Devolucion
+SELECT * FROM Almacen.Producto
+SELECT * FROM Almacen.Producto
+SELECT * FROM Almacen.DetalleDevolucion 
+
+DELETE FROM Almacen.DetalleDevolucion  WHERE iddevolucion=1 AND  idproducto=3
+
+ /***triggers***/
+ --Trigger de Stock
 CREATE OR REPLACE FUNCTION tr_stock_articulo_almacen()
   RETURNS TRIGGER AS
 $BODY$
@@ -192,6 +191,68 @@ CREATE TRIGGER tr_stock_articulo_DETTALEVENTA AFTER INSERT OR UPDATE OR DELETE
 ON Transaccion.DetalleVenta FOR EACH ROW
 EXECUTE PROCEDURE tr_stock_articulo_almacen2();
 
+/*********************/
+-----Trigger Edad
+CREATE FUNCTION EdadAPersona()
+  RETURNS TRIGGER AS
+$$
+    DECLARE 
+BEGIN
+	IF TG_OP = 'INSERT' THEN
+		UPDATE Almacen.Vendedor SET Edad = (SELECT EXTRACT(YEAR FROM current_date) - EXTRACT(YEAR FROM NEW.FechaNac)) WHERE IdVendedor = NEW.IdVendedor;
+	END IF;
+	RETURN NULL;
+END;
+$$
+  LANGUAGE plpgsql;
+
+ 
+CREATE FUNCTION EdadAPersona2()
+  RETURNS TRIGGER AS
+$$
+    DECLARE 
+BEGIN
+	IF TG_OP = 'INSERT' THEN
+		UPDATE Transaccion.Cliente SET Edad = (SELECT EXTRACT(YEAR FROM current_date) - EXTRACT(YEAR FROM NEW.FechaNac)) WHERE IdVendedor = NEW.IdVendedor;
+	END IF;
+	RETURN NULL;
+END;
+$$
+  LANGUAGE plpgsql;
+
+CREATE TRIGGER triGen_edad AFTER INSERT OR UPDATE 
+ON Almacen.Vendedor FOR EACH ROW
+EXECUTE PROCEDURE EdadAPersona();
+
+CREATE TRIGGER triGen_edad AFTER INSERT OR UPDATE 
+ON Transaccion.Cliente FOR EACH ROW
+EXECUTE PROCEDURE EdadAPersona2();
+
+INSERT INTO Almacen.Vendedor (Nombre,Domicilio,Email,Telefono,FechaNac) VALUES ('Alejnadro','Salvadro','ya@dfsfse','78665678','05/10/1996');
+SELECT * FROM Almacen.Vendedor;
+
+/*********************/
+------Trigger Subtotal------
+CREATE FUNCTION CalculaSubtotal()
+  RETURNS TRIGGER AS
+$$
+    DECLARE 
+BEGIN
+	IF TG_OP = 'INSERT' THEN
+		UPDATE Transaccion.DetalleVenta SET Subtotal = Subtotal + (NEW.cantidad ) WHERE IdDevolucion = NEW.IdDevolucion;
+ 
+	ELSEIF TG_OP = 'UPDATE' THEN
+		UPDATE Transaccion.DetalleVenta SET Subtotal = Subtotal - (OLD.cantidad ) WHERE IdDevolucion = OLD.IdDevolucion;
+ 
+	ELSEIF TG_OP = 'DELETE' THEN
+		UPDATE Transaccion.DetalleVenta SET Subtotal = Subtotal - (OLD.cantidad ) WHERE IdDevolucion = OLD.IdDevolucion;
+	END IF;
+ 
+	RETURN NULL;
+END
+$$
+LANGUAGE plpgsql;
+												     
 ---------------------------------------usuarios--------------------------------------------------
 CREATE USER Administrador WITH LOGIN ENCRYPTED PASSWORD '123';
 GRANT CONNECT ON DATABASE "Proyecto" TO Administrador;
@@ -213,5 +274,3 @@ GRANT SELECT ON ALL TABLES IN SCHEMA transaccion,almacen TO Empleado;
 GRANT INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA transaccion TO Empleado;
 REVOKE INSERT,UPDATE,DELETE ON TABLE transaccion.cliente FROM Empleado;
 GRANT INSERT,UPDATE,DELETE ON almacen.detalledevolucion,almacen.devolucion TO Empleado;
-
-select * from almacen.devolucion;
